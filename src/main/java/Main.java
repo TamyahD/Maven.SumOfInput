42523027101166b0cf3342c4bb6f8ad15c280113
Myah <myah@Zipcoders-MacBook-Pro-61.local>
2022-06-28 10:58:24 -0400

    updated pom file & completed lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
+        Main m = new Main();
+        m.sumNums();
+    }
 
+
+    public void sumNums() {
+        int max = getUserNum();
+        int sum = 0;
+
+        for (int i=0; i<= max; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+
+    public int getUserNum() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number: ");
+        int n = sc.nextInt();
+
+        return n;
     }
 }
